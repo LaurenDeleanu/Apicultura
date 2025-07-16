e61b69213b25fbe495b94bea7f31aebc3b98dd88
Lauurentiu Deleanu <107888381+LaurenDeleanu@users.noreply.github.com>
2025-07-16 19:01:59 +0000

    modificamos comamands y subimos "crear admin"

--- src/api/commands.py
+++ src/api/commands.py
@@ -1,34 +1,16 @@
+from .models import db, Usuario
 
-import click
-from api.models import db, User
-
-"""
-In this file, you can add as many commands as you want using the @app.cli.command decorator
-Flask commands are usefull to run cronjobs or tasks outside of the API but sill in integration 
-with youy database, for example: Import the price of bitcoin every night as 12am
-"""
-def setup_commands(app):
+def crear_usuario_admin():
+    admin = Usuario.query.filter_by(email='admin@apicultura.com').first()
     
-    """ 
-    This is an example command "insert-test-users" that you can run from the command line
-    by typing: $ flask insert-test-users 5
-    Note: 5 is the number of users to add
-    """
-    @app.cli.command("insert-test-users") # name of our command
-    @click.argument("count") # argument of out command
-    def insert_test_users(count):
-        print("Creating test users")
-        for x in range(1, int(count) + 1):
-            user = User()
-            user.email = "test_user" + str(x) + "@test.com"
-            user.password = "123456"
-            user.is_active = True
-            db.session.add(user)
-            db.session.commit()
-            print("User: ", user.email, " created.")
-
-        print("All test users created")
-
-    @app.cli.command("insert-test-data")
-    def insert_test_data():
-        pass
+    if not admin:
+        admin = Usuario(
+            email='admin@apicultura.com',
+            nombre='Administrador',
+            apellido='Sistema',
+            rol='admin'
+        )
+        admin.set_password('Admin123!')
+        db.session.add(admin)
+        db.session.commit()
+        print("Usuario administrador creado: admin@apicultura.com / Admin123!")
