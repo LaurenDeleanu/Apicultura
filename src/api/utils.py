fc7bb62c77ed1ba14a34bd279f27e1a1e5421e92
Lauurentiu Deleanu <107888381+LaurenDeleanu@users.noreply.github.com>
2025-07-17 18:05:45 +0000

    reescribimos utils.py

--- src/api/utils.py
+++ src/api/utils.py
@@ -1,5 +1,8 @@
 import re
+import json
+from flask import Response
 
+# -------- VALIDADORES --------
 def validar_email(email):
     patron = r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$'
     return re.match(patron, email) is not None
@@ -14,3 +17,30 @@ def validar_password(password):
     if not re.search(r'\d', password):
         return False, "La contraseña debe contener al menos un número"
     return True, "Contraseña válida"
+
+# -------- API Exception --------
+class APIException(Exception):
+    def __init__(self, message, status_code=400, payload=None):
+        Exception.__init__(self)
+        self.message = message
+        self.status_code = status_code
+        self.payload = payload
+
+    def to_dict(self):
+        rv = dict(self.payload or ())
+        rv['message'] = self.message
+        return rv
+
+# -------- Sitemap Generator --------
+def generate_sitemap(app):
+    links = []
+    for rule in app.url_map.iter_rules():
+        if rule.endpoint != 'static':
+            methods = ','.join(rule.methods)
+            url = str(rule)
+            links.append({
+                "endpoint": rule.endpoint,
+                "methods": methods,
+                "url": url
+            })
+    return json.dumps(links, indent=4)
