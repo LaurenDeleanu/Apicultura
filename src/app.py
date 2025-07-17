50b6c87d20a23c4618025ef54574ccf23840ad46
Lauurentiu Deleanu <107888381+LaurenDeleanu@users.noreply.github.com>
2025-07-17 18:17:13 +0000

    from api.admin import setup_admin

--- src/app.py
+++ src/app.py
@@ -8,7 +8,6 @@ from flask_swagger import swagger
 from api.utils import APIException, generate_sitemap
 from api.models import db
 from api.routes import api
-from api.admin import setup_admin
 from api.commands import setup_commands
 
 # from models import Person
